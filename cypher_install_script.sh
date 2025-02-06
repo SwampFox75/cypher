1c4bc519000041e01a4275705a973c9b98047c54
SwampFox75 <127417182+SwampFox75@users.noreply.github.com>
2025-02-06 15:03:41 -0500

    Update cypher_install_script.sh

--- cypher_install_script.sh
+++ cypher_install_script.sh
@@ -2,6 +2,14 @@
 
 echo "CyPhER Baseline Install Script 20250206"
 
+# install all package dependencies
 sudo DEBIAN_FRONTEND=noninteractive apt -y install git build-essential cmake openssl libssl-dev gcc pkg-config libpcap-dev libz-dev
 
-git clone https://github.com/DrTimothyAldenDavis/GraphBLAS.git 
+# download and install GraphBLAS
+path_graphblas="$HOME/Downloads/GraphBLAS"
+echo "Creating directory GraphBLAS in Downloads"
+mkdir $path_graphblas
+git clone https://github.com/DrTimothyAldenDavis/GraphBLAS.git $path_graphblas
+make -C $path_graphblas
+sudo make -C $path_graphblas install
+make -C $path_graphblas clean
