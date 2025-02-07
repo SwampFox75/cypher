c154ce2abf331dfcbc64e9509dd4913376cfe58f
SwampFox75 <127417182+SwampFox75@users.noreply.github.com>
2025-02-07 10:48:03 -0500

    Update cypher_install_script.sh

--- cypher_install_script.sh
+++ cypher_install_script.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
+# this is starting out as a simple script without any checks of any kind like error or existance checks
+
+# write to console the script is starting
 echo "CyPhER Baseline Install Script 20250206"
 
 # install all package dependencies
@@ -13,3 +16,26 @@ git clone https://github.com/DrTimothyAldenDavis/GraphBLAS.git $path_graphblas
 make -C $path_graphblas
 sudo make -C $path_graphblas install
 make -C $path_graphblas clean
+
+# download and install cryptopANT
+path_cryptopant="$HOME/Downloads/cryptopANT"
+echo "Creating directory cryptopANT in Downloads"
+mkdir $path_cryptopant
+wget -P ${path_cryptopant}/ https://ant.isi.edu/software/cryptopANT/cryptopANT-1.3.1.tar.gz
+tar -xzf ${path_cryptopant}/cryptopANT-1.3.1.tar.gz -C $path_cryptopant
+(cd $HOME/Downloads/cryptopANT/cryptopANT-1.3.1 && ./configure --with-scramble_ips)
+make -C ${path_cryptopant}/cryptopANT-1.3.1
+sudo make -C ${path_cryptopant}/cryptopANT-1.3.1 install
+make -C ${path_cryptopant}/cryptopANT-1.3.1 clean
+
+# download and install ILANDS (CyPhER)
+path_ilands="$HOME/Downloads/ILANDS"
+echo "Creating directory ILANDS in Downloads"
+mkdir $path_ilands
+git clone https://github.com/CAIDA/ILANDS-sensor.git $path_ilands
+make -C $path_ilands
+sudo make -C $path_ilands install
+make -C $path_ilands clean
+
+# run ldconfig to update shared libraries
+sudo ldconfig
