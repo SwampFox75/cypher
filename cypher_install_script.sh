d6d846707ac788bd3e939deb528bc014a061dd86
SwampFox75 <127417182+SwampFox75@users.noreply.github.com>
2025-02-20 10:29:18 -0500

    updated echo to console to reflect previous repo download changes

--- cypher_install_script.sh
+++ cypher_install_script.sh
@@ -10,7 +10,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt -y install git build-essential cmake ope
 
 # download and install GraphBLAS
 path_graphblas="/usr/local/src/GraphBLAS"
-echo "Creating directory GraphBLAS in Downloads"
+echo "Creating directory GraphBLAS in /usr/local/src/"
 mkdir $path_graphblas
 git clone https://github.com/DrTimothyAldenDavis/GraphBLAS.git $path_graphblas
 make -C $path_graphblas
@@ -19,7 +19,7 @@ make -C $path_graphblas clean
 
 # download and install cryptopANT
 path_cryptopant="/usr/local/src/cryptopANT"
-echo "Creating directory cryptopANT in Downloads"
+echo "Creating directory cryptopANT in /usr/local/src/"
 mkdir $path_cryptopant
 wget -P ${path_cryptopant}/ https://ant.isi.edu/software/cryptopANT/cryptopANT-1.3.1.tar.gz
 tar -xzf ${path_cryptopant}/cryptopANT-1.3.1.tar.gz -C $path_cryptopant
@@ -30,7 +30,7 @@ make -C ${path_cryptopant}/cryptopANT-1.3.1 clean
 
 # download and install ILANDS (CyPhER)
 path_ilands="/usr/local/src/ILANDS"
-echo "Creating directory ILANDS in Downloads"
+echo "Creating directory ILANDS in /usr/local/src/"
 mkdir $path_ilands
 git clone https://github.com/CAIDA/ILANDS-sensor.git $path_ilands
 make -C $path_ilands
