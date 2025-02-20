b7addafb1fb1aef452df79ca55f25235392c3bae
SwampFox75 <127417182+SwampFox75@users.noreply.github.com>
2025-02-20 10:26:59 -0500

    changed repo download from $HOME/Downloads to /usr/local/src

--- cypher_install_script.sh
+++ cypher_install_script.sh
@@ -9,7 +9,7 @@ echo "CyPhER Baseline Install Script 20250206"
 sudo DEBIAN_FRONTEND=noninteractive apt -y install git build-essential cmake openssl libssl-dev gcc pkg-config libpcap-dev libz-dev
 
 # download and install GraphBLAS
-path_graphblas="$HOME/Downloads/GraphBLAS"
+path_graphblas="/usr/local/src/GraphBLAS"
 echo "Creating directory GraphBLAS in Downloads"
 mkdir $path_graphblas
 git clone https://github.com/DrTimothyAldenDavis/GraphBLAS.git $path_graphblas
@@ -18,18 +18,18 @@ sudo make -C $path_graphblas install
 make -C $path_graphblas clean
 
 # download and install cryptopANT
-path_cryptopant="$HOME/Downloads/cryptopANT"
+path_cryptopant="/usr/local/src/cryptopANT"
 echo "Creating directory cryptopANT in Downloads"
 mkdir $path_cryptopant
 wget -P ${path_cryptopant}/ https://ant.isi.edu/software/cryptopANT/cryptopANT-1.3.1.tar.gz
 tar -xzf ${path_cryptopant}/cryptopANT-1.3.1.tar.gz -C $path_cryptopant
-(cd $HOME/Downloads/cryptopANT/cryptopANT-1.3.1 && ./configure --with-scramble_ips)
+(cd /usr/local/src/cryptopANT/cryptopANT-1.3.1 && ./configure --with-scramble_ips)
 make -C ${path_cryptopant}/cryptopANT-1.3.1
 sudo make -C ${path_cryptopant}/cryptopANT-1.3.1 install
 make -C ${path_cryptopant}/cryptopANT-1.3.1 clean
 
 # download and install ILANDS (CyPhER)
-path_ilands="$HOME/Downloads/ILANDS"
+path_ilands="/usr/local/src/ILANDS"
 echo "Creating directory ILANDS in Downloads"
 mkdir $path_ilands
 git clone https://github.com/CAIDA/ILANDS-sensor.git $path_ilands
