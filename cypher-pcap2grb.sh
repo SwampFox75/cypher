9dc42c875a5712339f622e2e7e89e2d504d5eb33
SwampFox75 <127417182+SwampFox75@users.noreply.github.com>
2025-05-05 15:29:43 -0400

    Update cypher-pcap2grb.sh

--- cypher-pcap2grb.sh
+++ cypher-pcap2grb.sh
@@ -2,26 +2,26 @@
 
 # create variables to store directory paths
 pcap_direcotry="/home/cypher-user/Desktop/cypher/pcaps"
-gbs_directory="/home/cypher-user/Desktop/cypher/gbs"
+grbs_directory="/home/cypher-user/Desktop/cypher/grbs"
 tmp_directory="/home/cypher-user/Desktop/cypher/tmp"
 
 # uncomment the lines below to clear the tmp directory of all previous runs each time to save space
 # rm $tmp_directory/*
 # $(date '+%b %d %Y %H:%M:%S') $(hostname) clearing tmp folder
 
-# script to take all but the latest pcap and convert it to gbs
+# script to take all but the latest pcap and convert it to grbs
 # get an array of all pcaps in the pcap folder sorted from newest to oldest
 pcap_list=($(find $pcap_direcotry -type f -name "*.pcap" | sort -nr))
 
 # get a count of the pcap files in the pcap folder
 pcap_count=${#pcap_list[@]}
 
-# if there is more than one pcap then process them to gbs
+# if there is more than one pcap then process them to grbs
 # if there is only one pcap it is probably being written to by tcpdump
 if [[ $pcap_count > 1 ]]; then
 	for pcap in ${pcap_list[@]:1}; do
-		echo "$(date '+%b %d %Y %H:%M:%S') $(hostname) running pcap2gbs on $pcap"
-		pcap2grb -i $pcap -o $gbs_directory
+		echo "$(date '+%b %d %Y %H:%M:%S') $(hostname) running pcap2grb on $pcap"
+		pcap2grb -i $pcap -o $grbs_directory 2>&1
 		echo "$(date '+%b %d %Y %H:%M:%S') $(hostname) moving $pcap to cypher/tmp"
 		mv $pcap $tmp_directory
 	done
